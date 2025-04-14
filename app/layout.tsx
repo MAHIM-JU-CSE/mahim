0e71fe3439b69efb294959edc44b02bf8e001ffd
MAHIM-JU-CSE <mahbubislammahim@gmail.com>
2025-04-14 15:41:42 +0600

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.svg?v=1" type="image/svg+xml" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThemeRegistry>
           {children}
